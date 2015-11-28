1571c8982207718650f1fa0b43cc11b0af566890
WillowLeaf <Leo.HLF@gmail.com>
2015-11-28 19:10:05 +0800

    Added intercept term before training.

--- lwlr.py
+++ lwlr.py
@@ -5,35 +5,35 @@ Locally-weighted logistic regression
 import numpy as np
 import matplotlib.pyplot as plt
 
-X_train = np.fromfile(r'E:\data\x.dat', sep = ' ').reshape((-1, 2))
-y_train = np.fromfile(r'E:\data\y.dat', sep = ' ').reshape((-1, 1))
-
-m = np.size(X_train, 0)
-n = np.size(X_train, 1)
-
-def predict(x):
+def predict(x, X_train, Y_train):    
+    """Returns the hypothesis of x given training set X_train and y_train.
+    """
+    m = np.size(X_train, 0)
+    n = np.size(X_train, 1)
     theta = np.zeros((n, 1))
     tau = 1
     lambda_ = 1e-4
     eps = 1e-6
-    
-    w = np.exp(-np.sum((X_train - x.T) ** 2, axis = 1) / (2 * tau))[:, np.newaxis]
+    w = np.exp(-np.sum((X_train - x.T) ** 2, axis = 1) / (2 * tau ** 2))[:, np.newaxis]
     g = np.ones((n, 1))
-    #i = 0
     while (g.T @ g)[0, 0] > eps:
-        #i += 1
-        # print ('theta#', i, '=', theta)
         h = 1 / (1 + np.exp(-(X_train @ theta)))
         D = np.diag((-w * h * (1 - h))[:, 0])
         H = X_train.T @ D @ X_train - np.identity(n) * lambda_
         g = X_train.T @ (w * (y_train - h)) - lambda_ * theta
         theta = theta - np.linalg.pinv(H) @ g
-        
     result = 1 / (1 + np.exp(-(x.T @ theta)))
-    print(theta)
     return True if result > 0.5 else False
-    
-if __name__ == '__main__':    
-    x0, x1 = input('Please input x: ').split()
-    x = np.array([[float(x0)], [float(x1)]])    
-    predict(x)
+
+def get_precision(X_train, y_train):
+    m = np.size(X_train, 0)
+    n = np.size(X_train, 1)
+    correct = sum(1 for i in range(m) if predict(X_train[i].reshape((n, 1)), X_train, y_train) == bool(y_train[i]))
+    return correct / m
+
+if __name__ == '__main__':        
+    X_train = np.fromfile(r'E:\data\x.dat', sep = ' ').reshape((-1, 2))
+    y_train = np.fromfile(r'E:\data\y.dat', sep = ' ').reshape((-1, 1))
+    print("Precision on training set (without intercept term):", get_precision(X_train, y_train))
+    X_train = np.hstack((np.ones((np.size(X_train, 0), 1)), X_train))
+    print("Precision on training set (with intercept term):", get_precision(X_train, y_train))
