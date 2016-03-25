53cadf35c2f9f0eba32beceae46684345d59c9f1
WillowLeaf <leo.hlf@gmail.com>
2016-03-25 16:59:15 +0800

    Add tmatrix to qa_clustering_util

--- qa_clustering_util.py
+++ qa_clustering_util.py
@@ -9,6 +9,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 import jieba
 import argparse
+import numpy as np
 import os
 
 def extract_qa_pairs(text):
@@ -22,18 +23,30 @@ def extract_qa_pairs(text):
 if __name__ == '__main__':    
     ap = argparse.ArgumentParser()
     ap.add_argument(dest = 'input_path', 
-                    help = 'file containing text to be clustered')
-    ap.add_argument(dest = 'output_dir',
-                    help = 'directory of the clustered result')
+                    help = 'file containing text to be clustered')                     
+    ap.add_argument(dest = 'output_dir', 
+                    help = 'directory to output the clustered result')
     ap.add_argument(dest = 'n_qst_clusters', type = int,
                     help = 'number of question clusters')
     ap.add_argument(dest = 'n_ans_clusters', type = int,
-                    help = 'number of answer clusters')
+                    help = 'number of answer clusters')               
+    ap.add_argument('-t', '--tmatrix', dest = 'tmatrix', action = 'store',
+                    help = 'path to output the transit matrix.')
+                    
     args = ap.parse_args()
     
     with open(args.input_path) as f:
         text = f.read()
     questions, answers = extract_qa_pairs(text)
+    
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+        
+    with open(os.path.join(args.output_dir, 'questions.txt'), 'w') as f:
+        f.write('\n'.join(questions))
+
+    with open(os.path.join(args.output_dir, 'answers.txt'), 'w') as f:
+        f.write('\n'.join(answers))
 
     print 'Clustering questions...'
     tv_qst = TfidfVectorizer()
@@ -42,7 +55,8 @@ if __name__ == '__main__':
     km_qst.fit(X_qst)      
     clustered_qst = [[] for i in xrange(args.n_qst_clusters)]    
     for i in xrange(len(questions)):
-        clustered_qst[km_qst.labels_[i]].append(questions[i])
+        clustered_qst[km_qst.labels_[i]].append(str(i))
+    
     question_cluster_dir = os.path.join(args.output_dir, 'question_clusters/')
     if not os.path.exists(question_cluster_dir):
         os.makedirs(question_cluster_dir)
@@ -54,8 +68,7 @@ if __name__ == '__main__':
             f.write('\n'.join(clustered_qst[i]))
         print 'Done'
     print 'All %d question clusters have been generated!' % args.n_qst_clusters
-    
-    
+        
     print 'Clustering answers...'  
     tv_ans = TfidfVectorizer()
     X_ans = tv_ans.fit_transform(map(lambda t: ' '.join(jieba.cut(t)), answers))
@@ -63,24 +76,28 @@ if __name__ == '__main__':
     km_ans.fit(X_ans)  
     clustered_ans = [[] for i in xrange(args.n_ans_clusters)]
     for i in xrange(len(answers)):
-        clustered_ans[km_ans.labels_[i]].append(i)
+        clustered_ans[km_ans.labels_[i]].append(str(i))
+    
     answer_cluster_dir = os.path.join(args.output_dir, 'answer_clusters/')
     if not os.path.exists(answer_cluster_dir):
         os.makedirs(answer_cluster_dir)
     for i in xrange(args.n_ans_clusters):
         print 'Generating answer cluster #%d..' % i,
-        answer_cluster_path = os.path.join(answer_cluster_dir,
-                                             str(i) + '.txt')
+        answer_cluster_path = os.path.join(answer_cluster_dir, str(i) + '.txt')
         with open(answer_cluster_path, 'w') as f:
-            f.write('\n'.join(clustered_qst[i]))
+            f.write('\n'.join(clustered_ans[i]))
         print 'Done!'
     print 'Done!'
     print 'All %d answer clusters have been generated!' % args.n_ans_clusters
 
-    with open(os.path.join(args.output_dir, 'questions.txt'), 'w') as f:
-        f.write('\n'.join(questions))
-
-    with open(os.path.join(args.output_dir, 'answers.txt'), 'w') as f:
-        f.write('\n'.join(answers))
-        
+    if args.tmatrix:
+        tmatrix = np.zeros((args.n_qst_clusters, args.n_ans_clusters))
+        for i in xrange(len(questions)):
+            tmatrix[km_qst.labels_[i], km_ans.labels_[i]] += 1
+        tmatrix /= np.sum(tmatrix, axis = 1, keepdims = True)
+        if args.tmatrix == 'txt':
+            np.savetxt(os.path.join(args.output_dir, 'tmatrix.txt'), tmatrix)
+        elif args.tmatrix == 'npy':
+            with open(os.path.join(args.output_dir, 'tmatrix.dat')) as f:
+                tmatrix.dump(f)
     print 'Success!'
